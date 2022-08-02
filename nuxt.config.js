2483a1c8ddbd9a88ada485404f9a22eccb73648f
Luan Andrade <luan.andrade@pullup.tech>
2022-08-02 19:27:10 -0300

    feat(plugin): added toasts plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    "~/plugins/bootstrap-icons.js"
+    "~/plugins/bootstrap-icons.js",
+    "~/plugins/toasts.js"
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
